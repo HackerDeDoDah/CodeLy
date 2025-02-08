b1149e694265b0c1d7384bca5e7b521496e56d57
Chris Day <chrisdaypro@protonmail.com>
2025-02-08 14:08:50 +0000

    first commit, codepen style editor

---
+++ scripts.js
@@ -0,0 +1,30 @@
+
+
+
+const htmlEditor = CodeMirror.fromTextArea(document.getElementById("html"), { mode: "xml", lineNumbers: true });
+const cssEditor = CodeMirror.fromTextArea(document.getElementById("css"), { mode: "css", lineNumbers: true });
+const jsEditor = CodeMirror.fromTextArea(document.getElementById("js"), { mode: "javascript", lineNumbers: true });
+
+function updatePreview() {
+    const html = htmlEditor.getValue();
+    const css = "<style>" + cssEditor.getValue() + "</style>";
+    const js = "<script>" + jsEditor.getValue() + "<\/script>";
+    const iframe = document.getElementById("preview");
+    
+    if (iframe.contentDocument) {
+        iframe.contentDocument.open();
+        iframe.contentDocument.write(html + css + js);
+        iframe.contentDocument.close();
+    }
+}
+
+[htmlEditor, cssEditor, jsEditor].forEach(editor => editor.on("change", updatePreview));
+
+    // Add shortcut for "!" to generate a basic HTML structure
+htmlEditor.on("inputRead", (editor, event) => {
+    if (editor.getValue().trim() === "!") {
+        editor.setValue(`<!DOCTYPE html>\n<html lang='en'>\n<head>\n    <meta charset='UTF-8'>\n    <meta name='viewport' content='width=device-width, initial-scale=1.0'>\n    <title>Document</title>\n</head>\n<body>\n    \n</body>\n</html>`);
+        editor.setCursor({line: 8, ch: 4}); // Move cursor inside body
+    }
+});
+
